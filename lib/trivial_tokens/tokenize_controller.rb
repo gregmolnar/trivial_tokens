201133dc851bdea8f5641290b64875f3b5187636
Thomas Millward Wright <thomas.wright@mwrinfosecurity.com>
2015-06-11 17:30:54 +0100

    add token methods to controller [WIP]

--- lib/trivial_tokens/tokenize_controller.rb
+++ lib/trivial_tokens/tokenize_controller.rb
@@ -10,14 +10,27 @@ module TrivialTokens
       def tokenize
 
         self.send( :define_method, "token_query" ) do
-          'foo'
+          if self.token_params[:q].present?
+            debugger
+            model = self.controller_name.classify.constantize
+            instance_variable_set(
+              "@#{self.controller_name}",
+              model.where("name like ?", "%#{self.token_params[:q]}%")
+            )
+            self.respond_to do |format|
+              format.json { render json: self.instance_variable_get("@#{self.controller_name}")}
+            end
+          else
+            yield
+          end
+
         end
 
         self.send( :define_method, "token_params" ) do
-          'foo'
+          self.params.permit(:q)
         end
 
-        self.around_action :token_query, only: :index
+        self.send(:around_action, :token_query, { only: :index } )
       end
     end
   end
