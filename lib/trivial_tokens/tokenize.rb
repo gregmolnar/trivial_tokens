a17d6b3cccdc7436861a9ec7b2462edb5ac77cd0
Thomas Millward Wright <thomas.wright@mwrinfosecurity.com>
2015-06-11 09:44:03 +0100

    Capitalise error message, strip whitespace

--- lib/trivial_tokens/tokenize.rb
+++ lib/trivial_tokens/tokenize.rb
@@ -5,7 +5,7 @@ module TrivialTokens
 
     extend ActiveSupport::Concern
 
-    included do 
+    included do
     end
 
     module ClassMethods
@@ -14,14 +14,14 @@ module TrivialTokens
         reflection = self.reflect_on_association(relation)
 
         if !reflection
-          raise TrivialTokens::Tokenize::UntokenizableAssociationError, "association #{relation} not found!"
+          raise TrivialTokens::Tokenize::UntokenizableAssociationError, "Association #{relation} not found!"
         elsif !(reflection.macro == :has_many || reflection.macro == :has_and_belongs_to_many)
           raise TrivialTokens::Tokenize::UntokenizableAssociationError,
-                "Expected assocation #{relation} to be of has_many or has_and_belongs_to_many, was #{reflection.macro}" 
+                "Expected assocation #{relation} to be of has_many or has_and_belongs_to_many, was #{reflection.macro}"
         end
 
         #relation is a symbol representing the relation to be tokenized
-        self.send( :define_method, "tokenized_#{relation.to_s}" ) do 
+        self.send( :define_method, "tokenized_#{relation.to_s}" ) do
           self.send("#{relation.to_s.singularize}_ids").join(',')
         end
 
